0da4b1d0a342d6e43f373b3dc296c70d94ed1939
Vlad <v.tsvetkov@pflb.ru>
2018-10-05 23:43:04 +0300

    logo, footer, style fixes

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -7,12 +7,13 @@ import {AppRoutingModel} from '../../app.routing';
 import { BioComponent } from './bio/bio.component';
 import { MusicComponent } from './music/music.component';
 import { PhotoComponent } from './photo/photo.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AppRoutingModel
   ],
-  declarations: [HomeComponent, HeaderComponent, ShellComponent, BioComponent, MusicComponent, PhotoComponent]
+  declarations: [HomeComponent, HeaderComponent, ShellComponent, BioComponent, MusicComponent, PhotoComponent, FooterComponent]
 })
 export class MainModule { }
